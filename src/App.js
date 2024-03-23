1438ac50b91fc7ae12d3e0c3299ab441a14fd91a
Szymon Skaskiewicz <szymon.skaskiewicz@gmail.com>
2024-03-23 16:19:18 +0200

    Added: Included Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import { Container } from 'react-bootstrap';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home/Home.js';
-import NavBar from './components/views/NavBar/NavBar.js';
+import Header from './components/views/Header/Header.js';
 import NotFound from './components/pages/NotFound/NotFound.js';
+import Footer from './components/views/Footer/Footer.js';
 
 const App = () => {
   return (
     <div>
-      <NavBar/>
+
       <Container>
+        <Header />
         <Routes>
           <Route exact path='/' element={< Home />}></Route>
-{/*           <Route path="/table/:tableId" element={<SingleTableDetails />} />
+          {/*           <Route path="/table/:tableId" element={<SingleTableDetails />} />
  */}          <Route path="*" element={<NotFound />} />
         </Routes>
+        <Footer />
       </Container>
     </div>
   )
