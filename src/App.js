91b924fecfa1c882082e90f262becf78b4f3554d
Szymon Skaskiewicz <szymon.skaskiewicz@gmail.com>
2024-03-23 14:54:47 +0200

    Added: Redux, initialState,  thunk

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { Container } from 'react-bootstrap';
-
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home/Home.js'
 const App = () => {
   return (
-    <Container>
-    </Container>
-  );
+    <div>
+      <Container>
+        <Routes>
+          <Route exact path='/' element={< Home />}></Route>
+        </Routes>
+
+      </Container>
+    </div>
+  )
 };
 
 export default App;
