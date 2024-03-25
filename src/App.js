67ea9e2f44233dd199431e358af14b4b9af1aee8
Szymon Skaskiewicz <szymon.skaskiewicz@gmail.com>
2024-03-25 10:52:23 +0200

    Added: Single table view

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Footer from './components/views/Footer/Footer.js';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchTables } from './redux/tablesRedux.js';
-
+import TableForm from './components/views/TableForm/TableForm.js';
 const App = () => {
 
   const dispatch = useDispatch();
@@ -20,9 +20,9 @@ const App = () => {
       <Container>
         <Header />
         <Routes>
-          <Route exact path='/' element={<Tables/>}></Route>
-          {/*           <Route path="/table/:tableId" element={<SingleTableDetails />} />
- */}          <Route path="*" element={<NotFound />} />
+          <Route exact path='/' element={<Tables />}></Route>
+          <Route path="/table/:tableId" element={<TableForm/>} />
+          <Route path="*" element={<NotFound />} /> 
         </Routes>
         <Footer />
       </Container>
