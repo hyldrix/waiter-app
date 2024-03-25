435bb31737fc14bc12d400e8329a69ecfb390736
Szymon Skaskiewicz <szymon.skaskiewicz@gmail.com>
2024-03-25 18:16:06 +0200

    Added: Added action creator, and implemented PUT ruest on update

--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -6,16 +6,34 @@ export const getTableById = (state, id) => state.tables.find(table => table.id =
 
 const createActionName = actionName => `app/tables/${actionName}`;
 const LOAD_TABLES = createActionName('LOAD_TABLES');
+const UPDATE_TABLE = createActionName('UPDATE_TABLE');
 
 // action creators 
 
 export const loadTables = payload => ({ type: LOAD_TABLES, payload })
+export const updateTable = payload => ({ type: UPDATE_TABLE, payload })
 
 export const fetchTables = () => {
     return (dispatch) => {
         fetch('http://localhost:3131/api/tables')
             .then(res => res.json())
             .then(tables => dispatch(loadTables(tables)))
+    }
+}
+
+export const updateTableRequest = (changedTable, tableId) => {
+    return (dispatch) => {
+        const options = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(changedTable)
+        }
+
+        fetch(`http://localhost:3131/api/tables/${tableId}`, options)
+        .then(res => res.json())
+        .then(data => dispatch(updateTable(data)))
     }
 }
 
